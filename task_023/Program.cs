55e1359790faa037bcc0b40f0a81988ad5a098a8
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-13 11:41:14 +0700

    Сделала task_023

---
+++ task_023/Program.cs
@@ -0,0 +1,17 @@
+﻿void Square(int value)
+{
+    for (int i = 1; i <= value; i++ )
+    {
+        int cub = (int) Math.Pow(i, 3);
+        Console.WriteLine(cub);
+    }
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int value = GetInput("Введите число: ");
+Square(value); 
