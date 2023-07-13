8f9092bf6a6debddae96fbb198fad5c27bd65849
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-13 11:40:51 +0700

    Сделала task_019

---
+++ task_019/Program.cs
@@ -0,0 +1,30 @@
+﻿void CheackPalindrom(string number)  
+{  
+    char[] firstNumber = number.ToCharArray();
+    char[] secondNumber = number.ToCharArray();
+    Array.Reverse(secondNumber);
+
+    string number1 = new String(firstNumber);
+    string number2 = new String(secondNumber);
+    
+    if (number1 == number2) Console.Write($"Число {number} палиндром!");  
+
+    else Console.Write($"Число {number} не палиндром!");
+}
+
+void Check(string number)
+{
+    if (number.Length > 5 | number.Length <5 ) 
+    Console.Write("Ошибка ввода!");
+    else
+    CheackPalindrom(number);
+}
+
+string GetInput(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+}
+
+string number = GetInput("Введите пятизначное число: ");
+Check(number); 
